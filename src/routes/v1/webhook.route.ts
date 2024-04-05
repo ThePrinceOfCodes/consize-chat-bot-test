d19dc0ece5bad0444233eb49d026a8e0d9c69b9f
theprinceofcodes <princejason109@gmail.com>
2024-04-05 13:40:04 +0100

    chore: wip

--- src/routes/v1/webhook.route.ts
+++ src/routes/v1/webhook.route.ts
@@ -5,6 +5,6 @@ import express, { Router } from "express";
 const router: Router = express.Router();
 
 router.get("/", courseControllers.webhook);
-
+router.post("/", courseControllers.postWebhook);
 
 export default router
